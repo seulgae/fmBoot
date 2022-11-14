43828848c54d3e7e8cbe965400327ac6f85a99f0
seulgae <r1605866@naver.com>
2022-11-15 03:57:47 +0900

    공지사항 게시판 업로드

--- src/main/java/com/ucamp/fm/controller/NoticeController.java
+++ src/main/java/com/ucamp/fm/controller/NoticeController.java
@@ -1,28 +1,101 @@
 package com.ucamp.fm.controller;
 
+import com.ucamp.fm.dto.NoticeDto;
+import com.ucamp.fm.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 
 @Controller
 @RequestMapping("/notice")
 public class NoticeController {
 
-    @GetMapping("/noticelist")
-    public String qnaForm() {
-        return "noticebbs/noticelist";
+    @Autowired
+    NoticeService noticeService;//리스트로 이동, 목록 출력
+
+    @GetMapping("/noticehome")
+    public String noticeList(Model model, HttpSession session) {
+//        if(m_id == null){
+//            m_id =
+//        }
+        String m_id = (String) session.getAttribute("m_id");
+        model.addAttribute("m_id",m_id);
+        model.addAttribute("notices", noticeService.noticelist());
+        return "noticebbs/noticehome";
     }
 
-    @GetMapping("/noticewrite")
-    public String qnaWrite() {
-        return "noticebbs/noticewrite";
-    }
 
-    @GetMapping("/noticeread")
-    public String qnaRead() {
+    @GetMapping("/noticeread/{n_no}")
+    public String noticeRead(Model model, @PathVariable("n_no") int n_no) {
+        model.addAttribute("notices", noticeService.noticeselect(n_no));
+        noticeService.countup(n_no);
         return "noticebbs/noticeread";
     }
 
+    @RequestMapping("/noticedelete/{n_no}")
+    public String noticeDelete(@PathVariable("n_no") int n_no) {
+        noticeService.noticedelete(n_no);
+        return "redirect:/notice/noticehome";
+    }
+    // 글쓰기 폼
+    @GetMapping("/noticewrite")
+    public String noticeWrite(HttpSession session) {
+        String n_id = (String) session.getAttribute("m_id");
+        if(n_id == null){
+            return "redirect:/login/login";
+        }
+        return "noticebbs/noticewrite";
+    }
+    // 글쓰기 입력
+    @PostMapping("/noticeinsert")
+    public String noticeInsert(@RequestParam("n_title") String n_title, HttpSession session,
+                               @RequestParam("n_content") String n_content) {
+        String n_id = (String) session.getAttribute("m_id");
+        HashMap<String, Object> map = new HashMap<String, Object>();
+
+        map.put("n_id", n_id);
+        map.put("n_title", n_title);
+        map.put("n_content", n_content);
+
+        noticeService.noticeinsert(map);
+        return "redirect:/notice/noticehome";
+    }
+    
+    // 글수정 폼
+    @GetMapping("/noticemod/{n_no}")
+    public String noticemod(@PathVariable("n_no") int n_no,
+                            HttpSession session, Model model) {
+        String n_id = (String) session.getAttribute("m_id");
+        if(n_id == null){
+            return "redirect:/login/login";
+        }
+        model.addAttribute("notice",noticeService.noticeselect(n_no));
+
+        return "noticebbs/noticemod";
+    }
+
+    @PostMapping("/noticemodac")
+    public String noticemod_ac(HttpSession session,
+                               @RequestParam("n_title") String n_title,
+                               @RequestParam("n_content") String n_content) {
+        String n_id = (String) session.getAttribute("m_id");
+        HashMap<String, Object> map = new HashMap<String, Object>();
+
+        map.put("n_id", n_id);
+        map.put("n_title", n_title);
+        map.put("n_content", n_content);
+
+        if(n_id == null){
+            return "redirect:/login/login";
+        }
+
+        noticeService.noticeinsert(map);
+        return "redirect:/notice/noticehome";
+    }
 }
 
---
+++ src/main/java/com/ucamp/fm/dto/NoticeDto.java
@@ -0,0 +1,28 @@
+package com.ucamp.fm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class NoticeDto {
+    private int n_no; // 글 순서
+    private String n_title; // 글 제목
+    private String n_id; // 작성자 아이디
+    private String n_content; // 글 내용
+
+    private String n_date; // 작성일자
+    private int n_count; // 조회수
+
+    public NoticeDto(String n_id, String n_title,String n_content){
+        this.n_id = n_id;
+        this.n_title = n_title;
+        this.n_content = n_content;
+    }
+}
