805faf2f63de9a26309c9809d065d58434a700b8
seulgae <r1605866@naver.com>
2022-11-15 15:22:12 +0900

    댓글 신고기능 추가

--- src/main/java/com/ucamp/fm/mapper/CmtMapper.java
+++ src/main/java/com/ucamp/fm/mapper/CmtMapper.java
@@ -3,6 +3,7 @@ package com.ucamp.fm.mapper;
 import com.ucamp.fm.dto.CmentDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -12,11 +13,16 @@ public interface CmtMapper {
     // 팀 블로그 댓글만 조회함.
     List<CmentDto> cmtlist(String c_no);
 
+    @Select("select * from cment where c_dec > 0")
+    List<CmentDto> cmtlistdec(CmentDto cmentDto);
+
+    @Update( "update cment SET C_dec = C_dec+1 WHERE c_no=#{c_no}")
+    void cmtdec(int c_no);
+
     void cmtinsert(String c_no, String c_c_id, String c_content);
 
     @Select("select * from cment where c_c_id=#{c_c_id}")
     CmentDto cmtone();
 
     void cmtdelete(String c_no);
-
 }
